f6e45f12730470286fec361a7ffd4b3f46d984c9
Lada-Novak <lada4445@yandex.ru>
2022-05-03 17:44:42 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,22 @@
 $(function () {
+
+  // slick
   $('.top-slider__inner').slick({
-    dots:true,
-    arrows:false,
-    fade:true,
-    autoplay:true,
-    autoplaySpead:2000,
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpead: 2000,
   });
 
+  // rateYo
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill:"#ccccce",
+    ratedFill:"#ffc35b",
+    readOnly: true
+  });
+
+  // mixitup
   var mixer = mixitup('.article');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -58,6 +58,7 @@ export const scripts = () => {
 			"node_modules/mixitup/dist/mixitup.min.js",
       "node_modules/slick-carousel/slick/slick.js",
       "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+      "node_modules/rateyo/src/jquery.rateyo.js",
 			"app/js/main.js",
 		])
 		.pipe(concat("main.min.js"))
