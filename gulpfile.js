a6882c2d3ab05cf6deb7155a09fca99d6d57fc29
Lada-Novak <lada4445@yandex.ru>
2022-05-03 15:20:22 +0200

    added video

--- gulpfile.js
+++ gulpfile.js
@@ -57,6 +57,7 @@ export const scripts = () => {
 			"node_modules/swiper/swiper-bundle.min.js",
 			"node_modules/mixitup/dist/mixitup.min.js",
       "node_modules/slick-carousel/slick/slick.js",
+      "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
 			"app/js/main.js",
 		])
 		.pipe(concat("main.min.js"))
