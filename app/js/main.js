b7a864ec7f87f9a399552586633b6e3af0e85d9f
Lada-Novak <lada4445@yandex.ru>
2022-05-06 19:33:42 +0200

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,33 @@
 $(function () {
 
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function(){
+    $('.product-item').addClass('product-item--list')
+  });
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list')
+  });
+  // styler
+  $('.select-style').styler();
+
+  // ion - rangeslider
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix:"$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   // slick
   $('.top-slider__inner').slick({
     dots: true,
